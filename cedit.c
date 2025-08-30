31d2feee0fe6a4f49fd34605bf912eccc5d86e69
chandan-wsl <chandanyuva1511@gmail.com>
2025-08-30 19:01:45 +0000

    Disable Ctrl-S and Ctrl-Q

--- cedit.c
+++ cedit.c
@@ -16,6 +16,7 @@ void enableRawMode(){
 	atexit(disableRawMode);
 	
 	struct termios raw = orig_termios;
+	raw.c_iflag &=~(IXON); // disable flow control
 	raw.c_lflag &=~(ECHO|ICANON|ISIG);
 	tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
 }
